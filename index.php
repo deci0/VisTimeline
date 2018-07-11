a7328de9a73d5693dc020105680beea2b158cf07
deci0 <martins.decio@gmail.com>
2018-07-11 14:59:49 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr-CH">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel='icon' type='image/png' href='favicon.png' />
+
+		<title>TIMELINE</title>
+
+		<link rel="stylesheet" type="text/css" href="http://visjs.org/dist/vis.css">
+
+		<script type="text/javascript" src="http://visjs.org/dist/vis.js"></script>
+
+	</head>
+	<body>
+
+	</body>
+</html>
